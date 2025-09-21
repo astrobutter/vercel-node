ba7022a21a68f1bd0751e98d2d9616692c3976c9
astrobutter <ayushsinghuk05@gmail.com>
2025-09-21 22:22:40 +0530

    routes add on index

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 import express from "express";
 import cors from "cors";
-import { MockRoutes } from "./Routes/MockRoutes/MockRoutes.js";
-import { DoctorModel } from "./models/Doctor.js";
 import mongoose from "mongoose";
-import { AppointmentModel } from "./models/Appointment.js";
+import { MockRoutes } from "./Routes/MockRoutes/MockRoutes.js";
+import { MongoRoutes } from "./Routes/MongoData/MongoData.js";
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true, credentials: true }));
@@ -19,17 +18,8 @@ app.get("/hello", (req, res) => {
   res.json({ message: `Hello, ${name}!` });
 });
 
-app.get("/doc-data", async (req, res) => {
-    const result = await DoctorModel.find();
-  res.json(result);
-});
-
-app.get("/appointment-data", async (req, res) => {
-    const result = await AppointmentModel.find();
-  res.json(result);
-});
-
 app.use("/mock", MockRoutes);
+app.use("/mongo", MongoRoutes);
 mongoose.connect("mongodb+srv://user001:test12345@cluster0.orisnk7.mongodb.net/medicare?",{ useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(3000, () => console.log(`Local server on http://localhost:3000`));
