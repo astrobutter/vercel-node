a4cc7116e0e0ff3369458cb8b2a5e380c2acce4b
astrobutter <ayushsinghuk05@gmail.com>
2025-09-22 15:50:02 +0530

    model import fix in mongo routes

--- Routes/MongoData/MongoData.js
+++ Routes/MongoData/MongoData.js
@@ -1,13 +1,15 @@
-import express from 'express';
+import express from "express";
+import { DoctorModel } from "../../models/Doctor.js";
+import { AppointmentModel } from "../../models/Appointment.js";
 const router = express.Router();
 
 router.get("/doc-data", async (req, res) => {
-    const result = await DoctorModel.find();
+  const result = await DoctorModel.find();
   res.json(result);
 });
 
 router.get("/appointment-data", async (req, res) => {
-    const result = await AppointmentModel.find();
+  const result = await AppointmentModel.find();
   res.json(result);
 });
 export { router as MongoRoutes };
